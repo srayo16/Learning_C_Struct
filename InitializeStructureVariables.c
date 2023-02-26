9c6de92905a8999879b9de36199fa4a45f9e1b9b
srayo16 <srayosikder@gmail.com>
2023-02-26 22:35:51 +0600

    initializing

--- InitializeStructureVariables.c
+++ InitializeStructureVariables.c
@@ -10,21 +10,13 @@ struct person
 };
 
 // Global Variable
-struct person person1, person2;
+struct person person1 = {"Srayo", 55, 3.98};
+
+struct person person2, person3;
 
 int main()
 
 {
-    // Local Struct
-    // struct person
-    // {
-    //     char name[20];
-    //     int x;
-    //     float y;
-    // };
-
-    // Local Variable
-    // struct person person1, person2;
 
     strcpy(person1.name, "srayo");
     person1.x = 45;
@@ -32,6 +24,8 @@ int main()
     strcpy(person2.name, "Mariam");
     person2.x = 40;
     person2.y = 45.22;
+
+    person3 = person2;
 
     printf("Person Couple: \n");
     printf("Name: %s\n", person1.name);
@@ -41,6 +35,10 @@ int main()
     printf("Name: %s\n", person2.name);
     printf("X: %d\n", person2.x);
     printf("Y: %.2f\n", person2.y);
+
+    printf("Name: %s\n", person3.name);
+    printf("X: %d\n", person3.x);
+    printf("Y: %.2f\n", person3.y);
 
     printf("Good Job!");
 
